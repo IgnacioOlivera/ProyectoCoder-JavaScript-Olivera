9fb8d0fe366b0a9b0fc7c0cd1e070b427c05cfaf
Ignacio Olivera <copi112014@gmail.com>
2024-07-29 23:24:32 -0300

    "primer entrega"

---
+++ assets/Js/index.js
@@ -0,0 +1,53 @@
+/* CONFIRMACION DE EDAD */
+var edad = window.prompt("ingrese su edad")
+
+
+if (edad > 17){ 
+    console.log("Bienvenido a la Casa de Cambio $$$!")
+}
+else{ console.log("Acceso denegado por incumplimiento de politicas.")
+
+}
+
+/*Casa de cambio - Conversor de dinero para peso Uruguayo.*/
+
+let libra = 52;
+let euro = 43;
+let dolar = 40;
+let real = 8;
+let yen = 1;
+
+let cantidad = 0
+let moneda = prompt("moneda")
+
+function convertir(cantidad, moneda){
+    
+    
+
+    if( moneda == libra ){ 
+        return ("£" +" "+ (cantidad=prompt("introduce un monto")) / libra)
+    }
+    if( moneda == euro ){
+        return ("€" +" "+ (cantidad=prompt("introduce un monto")) / euro)
+     }
+     
+    if(  moneda == dolar){
+        return ("USD" +" "+ (cantidad=prompt("introduce un monto")) / dolar)
+     }
+     
+    if(  moneda == real ){
+        return ("R$" +" "+ (cantidad=prompt("introduce un monto")) / real)
+     }
+     
+    if( moneda == yen ){
+        return ("¥" +" "+ (cantidad=prompt("introduce un monto")) / yen)
+     }
+
+    else{ 
+        return "No existe ese tipo de cambio "
+    }
+    
+   
+}
+console.log(convertir(cantidad, moneda))
+
