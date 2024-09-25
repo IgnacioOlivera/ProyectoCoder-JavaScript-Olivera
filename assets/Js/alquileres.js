a4fb108883d1b1f77cdba17dbc143ccd247548ee
Ignacio Olivera <copi112014@gmail.com>
2024-09-25 07:06:19 -0300

    x

--- assets/Js/alquileres.js
+++ assets/Js/alquileres.js
@@ -1,41 +1,41 @@
 //* CREO MIS TIPOS DE ALQUILERES  *//
-const alquileres = [ 
+// const alquileres = [ 
     
-    { 
-        id:1,
-        nombre: "Casas",
-        img: "../assests//img/casas.jpeg",
-        precio: " USD 300 ",
-    },
-    { 
-        id:2,
-        nombre: "Hoteles",
-        img: "../assets/img/hotel.jpeg",
-        precio: " USD 200 ",
-    },
-    { 
-        id:3,
-        nombre: "Airbnbs",
-        img: "../assets/img/Airbnb.jpeg",
-        precio: " USD 150 ",
-    },
-    { 
-        id:4,
-        nombre: "Departamentos",
-        img: "../assets/img/departamento.jpeg",
-        precio: " USD 150 ",
-    },
-    { 
-        id:5,
-        nombre: "Casas Rodantes",
-        img: "../assets/img/casaRodante.jpeg",
-        precio: " USD 100 ",
-    },
-    { 
-        id:6,
-        nombre: "Campings",
-        img: "../assets/img/camping.jpeg",
-        precio: " USD 50 ",
-    }
+//     { 
+//         id:1,
+//         nombre: "Casas",
+//         img: "../assests//img/casas.jpeg",
+//         precio: " USD 300 ",
+//     },
+//     { 
+//         id:2,
+//         nombre: "Hoteles",
+//         img: "../assets/img/hotel.jpeg",
+//         precio: " USD 200 ",
+//     },
+//     { 
+//         id:3,
+//         nombre: "Airbnbs",
+//         img: "../assets/img/Airbnb.jpeg",
+//         precio: " USD 150 ",
+//     },
+//     { 
+//         id:4,
+//         nombre: "Departamentos",
+//         img: "../assets/img/departamento.jpeg",
+//         precio: " USD 150 ",
+//     },
+//     { 
+//         id:5,
+//         nombre: "Casas Rodantes",
+//         img: "../assets/img/casaRodante.jpeg",
+//         precio: " USD 100 ",
+//     },
+//     { 
+//         id:6,
+//         nombre: "Campings",
+//         img: "../assets/img/camping.jpeg",
+//         precio: " USD 50 ",
+//     }
 
-]
+// ]
--- assets/Js/serviciosStorage.js
+++ assets/Js/serviciosStorage.js
@@ -1,27 +1,27 @@
 //*  Storage & JSON *//
 
-function guardarReserva(producto){ 
-    const data = JSON.parse(localStorage.getItem("bicicletas"))|| [];
-    console.log(data);
-    if(!data){
-        const nuevoProducto= producto;
-        nuevoProducto.cantidad = 1;
-        localStorage.setItem("alquileres",JSON.stringify([nuevoProducto]));
-    }else{
-        const indiceProducto = data.findIndex(alquiler => alquiler.id === alquiler.id);
-        console.log(indiceProducto)
-        const nuevaData = data;
-     if (indiceProducto === -1) { 
-        nuevaData.push(obtenerNuevoProductoParaData(producto))
-      }else{
-        nuevaData[indiceProducto].cantidad ++;
-      }
-      localStorage.setItem("alquileres",JSON.stringify(nuevaData))
-    }
-}
+// function guardarReserva(producto){ 
+//     const data = JSON.parse(localStorage.getItem("bicicletas"))|| [];
+//     console.log(data);
+//     if(!data){
+//         const nuevoProducto= producto;
+//         nuevoProducto.cantidad = 1;
+//         localStorage.setItem("alquileres",JSON.stringify([nuevoProducto]));
+//     }else{
+//         const indiceProducto = data.findIndex(alquiler => alquiler.id === alquiler.id);
+//         console.log(indiceProducto)
+//         const nuevaData = data;
+//      if (indiceProducto === -1) { 
+//         nuevaData.push(obtenerNuevoProductoParaData(producto))
+//       }else{
+//         nuevaData[indiceProducto].cantidad ++;
+//       }
+//       localStorage.setItem("alquileres",JSON.stringify(nuevaData))
+//     }
+// }
 
-function obtenerNuevoProductoParaData(producto){
-    const nuevoProducto = producto
-    nuevoProducto.cantidad = 1;
-    return nuevoProducto;
-}
+// function obtenerNuevoProductoParaData(producto){
+//     const nuevoProducto = producto
+//     nuevoProducto.cantidad = 1;
+//     return nuevoProducto;
+// }
